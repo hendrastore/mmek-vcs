cc33b1b6cc1b80c00b03febae9a30e6c3f512c41
hendrastore <102216067+hendrastore@users.noreply.github.com>
2022-09-02 18:09:37 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoread = false // auto read pesan / message
 //global.available = false //status online (online)
 
 // Other
-global.botname = "Hendra-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ HendraOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtu.be/uxV-G9v5rPs" //bebas asal jan hapus
-global.github = "https://github.com/hendrastore" //bebas
+global.botname = "EZZY STORE" //namabot kalian
+global.ownername= "ᴹᴿ᭄ EZZY STORE ×፝֟͜×" //nama kalian
+global.myweb ="" //bebas asal jan hapus
+global.youtube = "" //bebas asal jan hapus
+global.github = "" //bebas
 global.email = "hendramaenmllb@gmail.com" //bebas
 global.region = "Indonesia" //bebas
-global.ownernomer = "6283181085070" // nomor wa kalian
-global.ownernomerr = "+6283181085070" //nmr wa kalian
+global.ownernomer = "+6282141809800" // nomor wa kalian
+global.ownernomerr = "+6282141809800" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6283181085070","6283181085070","6283181085070"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Hendra-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh Hendra-md' //sticker wm ganti nama kalian
+global.owner = ["6282141809800","6282141809800","6282141809800"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© EZY STORE' //sticker wm ubah
+global.author = 'Di Buat Oleh EZY STORE' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
